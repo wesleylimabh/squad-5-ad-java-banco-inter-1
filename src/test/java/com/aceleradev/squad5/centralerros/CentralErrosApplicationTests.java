2ed49d93fa493ac9e236295a80bc2b49841aefc4
wesleylimabh <wesleylima@gmx.com>
2019-12-04 08:03:26 -0300

    start inicial com Spring Boot

---
+++ src/test/java/com/aceleradev/squad5/centralerros/CentralErrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aceleradev.squad5.centralerros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralErrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
