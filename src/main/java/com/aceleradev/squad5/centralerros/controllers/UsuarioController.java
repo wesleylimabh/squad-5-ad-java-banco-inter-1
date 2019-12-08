221c18f0fb840eeb74b2ff86d400f9cc1667d66f
Rafael <rcalvares@outlook.com>
2019-12-08 17:34:05 -0300

    usuario controller

--- src/main/java/com/aceleradev/squad5/centralerros/controllers/UsuarioController.java
+++ src/main/java/com/aceleradev/squad5/centralerros/controllers/UsuarioController.java
@@ -1,7 +1,9 @@
 package com.aceleradev.squad5.centralerros.controllers;
 
+import com.aceleradev.squad5.centralerros.entity.Usuario;
 import com.aceleradev.squad5.centralerros.service.interfaces.UsuarioServiceInterface;
 import io.swagger.annotations.Api;
+import net.bytebuddy.utility.RandomString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,16 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Random;
 
 @RestController
 @Api(tags = "Usuario", description = "Endpoints para gerenciamento de usuários")
 @RequestMapping("/usuario")
-public class CadastroController {
+public class UsuarioController {
 
     private final UsuarioServiceInterface usuarioServiceInterface;
 
     @Autowired
-    public CadastroController(UsuarioServiceInterface usuarioServiceInterface) {
+    public UsuarioController(UsuarioServiceInterface usuarioServiceInterface) {
         this.usuarioServiceInterface = usuarioServiceInterface;
     }
 
@@ -30,5 +33,21 @@ public class CadastroController {
         return ResponseEntity.ok().build();
     }
 
+    @PostMapping("/cadastro")
+    public ResponseEntity<Void> cadastro(@Valid @RequestHeader String email,
+                                         @Valid @RequestHeader String senha,
+                                         @Valid @RequestHeader String nome){
+
+        Usuario usuario = new Usuario();
+
+        usuario.setEmail(email);
+        usuario.setNome(nome);
+        usuario.setSenha(senha);
+        usuario.setToken(RandomString.hashOf(20));
+
+        usuarioServiceInterface.save(usuario);
+
+        return ResponseEntity.ok().build();
+    }
 
 }
