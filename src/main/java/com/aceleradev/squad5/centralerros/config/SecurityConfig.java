af43c9bbed6b8971c6098b0048b22a63c9b00a5f
wesleylimabh <wesleylima@gmx.com>
2019-12-10 03:20:22 -0300

    Implementado segurança com OAuth2

---
+++ src/main/java/com/aceleradev/squad5/centralerros/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.aceleradev.squad5.centralerros.config;
+
+import com.aceleradev.squad5.centralerros.service.impl.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableWebSecurity
+@EnableAuthorizationServer
+@EnableResourceServer
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomUserDetailService userDetailService;
+
+    @Autowired
+    public SecurityConfig(CustomUserDetailService userDetailService) {
+        this.userDetailService = userDetailService;
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
+        http.authorizeRequests()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into usuario(nome, email, senha, token) values ('teste', 'teste@test.com', '$2a$10$RCY3VHJbXq12w4evcO9TEuYfBsC.aAyaYTJ1M2LEKSghp.oGlBsnu', 'bXq12w4evcO9TEuYfBsC');
+
+insert into erro(ambiente, level, descricao, data_hora, titulo, origem, eventos, arquivado) values (1, 1, 'Descrição completa do erro', '01/01/2019 09:53:12', 'titulo do erro', 'servidor/serviço origem do erro', 234, 0);
+insert into erro(ambiente, level, descricao, data_hora, titulo, origem, eventos, arquivado) values (1, 2, 'Descrição completa do erro', '24/05/2019 09:53:12', 'titulo do erro', 'servidor/serviço origem do erro', 432, 0);
+insert into erro(ambiente, level, descricao, data_hora, titulo, origem, eventos, arquivado) values (2, 1, 'Descrição completa do erro', '15/07/2019 09:53:12', 'titulo do erro', 'servidor/serviço origem do erro', 123, 0);
+insert into erro(ambiente, level, descricao, data_hora, titulo, origem, eventos, arquivado) values (3, 3, 'Descrição completa do erro', '30/11/2019 09:53:12', 'titulo do erro', 'servidor/serviço origem do erro', 456, 0);
