7070a4aa9c51ccb86178f86640ed668116623402
Rafael <rcalvares@outlook.com>
2019-12-10 22:06:34 -0300

    adicionando autenticacao

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,5 @@ insert into erro (ambiente, arquivado, coletor, data_hora, descricao, eventos, l
 insert into erro (ambiente, arquivado, coletor, data_hora, descricao, eventos, level, origem, titulo, id) values (0, false, 'RAFAEL', now(), 'teste', 100, 0, '10.01.2', 'teste', 3)
 insert into erro (ambiente, arquivado, coletor, data_hora, descricao, eventos, level, origem, titulo, id) values (0, false, 'RAFAEL', now(), 'teste', 100, 0, '10.01.2', 'teste', 4)
 insert into erro (ambiente, arquivado, coletor, data_hora, descricao, eventos, level, origem, titulo, id) values (0, false, 'RAFAEL', now(), 'teste', 100, 0, '10.01.2', 'teste', 5)
+
+insert into usuario(nome, email, senha, token) values ('teste', 'teste@test.com', '$2a$10$RCY3VHJbXq12w4evcO9TEuYfBsC.aAyaYTJ1M2LEKSghp.oGlBsnu', 'bXq12w4evcO9TEuYfBsC');
