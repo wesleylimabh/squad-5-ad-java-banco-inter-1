5177c3ff81cd8a2177336da8cc71f64cea5af57d
Oséias da Silva Rocha <oseiasrocha@gmail.com>
2019-12-20 01:38:59 -0300

    Atualizando usuario controler e DTO

--- src/main/java/com/aceleradev/squad5/centralerros/dto/UsuarioDto.java
+++ src/main/java/com/aceleradev/squad5/centralerros/dto/UsuarioDto.java
@@ -13,23 +13,6 @@ public class UsuarioDto {
     private Long id;
     private String nome;
     private String email;
-<<<<<<< HEAD
-//    private String senha;
-//    private String token;
-
-//    public Usuario toEntity (){
-//
-//        return Usuario.builder()
-//                .id(this.id)
-//                .nome(this.nome)
-//                .email(this.email)
-////                .senha(CriptografiaUtil.criptografa(this.senha))
-////                .token(this.token)
-//                .build();
-//    }
-
-=======
     private String token;
 
->>>>>>> c4c7f4347d8dab2e17633f19ef67bcff5d978b8a
 }
