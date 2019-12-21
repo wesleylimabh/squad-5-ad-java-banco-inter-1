2f3d23184e7f4fb32057ee5483e137549977cda0
wesleylimabh <wesleylima@gmx.com>
2019-12-21 00:15:38 -0300

    incluído dados para testes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-insert into erro (ambiente, arquivado, coletor, data_hora, descricao, eventos, level, origem, titulo, id, detalhes) values (0, false, 'RAFAEL', now(), 'teste1', 100, 0, '10.01.2', 'erro 1', 1,'ocorreu de madrugada')
+insert into erro (ambiente, arquivado, coletor, data_hora, descricao, eventos, level, origem, titulo, id, detalhes) values (0, false, 'RAFAEL', now(), '"org.h2.engine.Session.prepareCommand(Session.java:595) ~[h2-1.4.200.jar:1.4.200]"', 100, 0, '10.01.2', 'erro 1', 1,'ocorreu de madrugada')
 insert into erro (ambiente, arquivado, coletor, data_hora, descricao, eventos, level, origem, titulo, id, detalhes) values (2, false, 'OSEIAS', now(), 'teste2', 100, 1, '10.01.4', 'erro 2', 2,'ocorreu pela manhã')
 insert into erro (ambiente, arquivado, coletor, data_hora, descricao, eventos, level, origem, titulo, id, detalhes) values (1, false, 'WESLEY', now(), 'teste3', 100, 2, '10.01.8', 'erro 3', 3,'ocorreu por 20 min')
 insert into erro (ambiente, arquivado, coletor, data_hora, descricao, eventos, level, origem, titulo, id, detalhes) values (0, false, 'RENATA', now(), 'teste4', 100, 1, '10.01.9', 'erro 4', 4,'ocorreu ao inserir novo usuário')
